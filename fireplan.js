eb7822ed6482d6a9a7de84c7e8e416f7b5d3cff9
Nash Reilly <nrc.reilly@gmail.com>
2017-05-22 22:13:03 -0400

    Rewrote futureValue as a standalone function. Added tables for 401k and Roth predictions.

--- fireplan.js
+++ fireplan.js
@@ -1,39 +1,43 @@
-$(document).ready(function() {
-	$('#button-id').click(function() {
-		var age     = $('#formAge').val();
-		var value   = $('#formValue').val();
-		var deposit = $('#formDeposit').val();
-		var pay_per = 24;
+function formatMoney(n) {
+    return '$' + n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g,'$1,');
+}
 
-		var total_lo   = value;
-		var total_av   = value;
-		var total_hi   = value;
-		var dataset = [];
-		for (var i = age; i <= 60; ++i) {
-			total_lo  = total_lo * 1.05;
-			total_av  = total_av * 1.07;
-			total_hi  = total_hi * 1.12;
+function futureValue(age, value, deposit, pay_per, retire) {
+	var total_lo   = value;
+	var total_av   = value;
+	var total_hi   = value;
 
-			accum_lo  = total_lo.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g,'$1,');
-			accum_av  = total_av.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g,'$1,');
-			accum_hi  = total_hi.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g,'$1,');
-			salary = (0.04 * total_av).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+    var dataset = [];
+	for (var i = age; i <= 60; ++i) {
+		total_lo  = total_lo * 1.05;
+		total_av  = total_av * 1.07;
+		total_hi  = total_hi * 1.12;
 
-			dataset.push([i , salary, accum_lo, accum_av, accum_hi]);
+		accum_lo  = formatMoney(total_lo); 
+		accum_av  = formatMoney(total_av);
+		accum_hi  = formatMoney(total_av);
 
-			total_lo  = total_lo + (deposit * pay_per);
-			total_av  = total_av + (deposit * pay_per);
-			total_hi  = total_hi + (deposit * pay_per);
-		}
+		salary = formatMoney((0.04 * total_av));
 
-		if ( $.fn.dataTable.isDataTable( '#example' ) ) {
-			table = $('#example').DataTable();
+		dataset.push([i , salary, accum_lo, accum_av, accum_hi]);
+
+		total_lo  = total_lo + (deposit * pay_per);
+		total_av  = total_av + (deposit * pay_per);
+		total_hi  = total_hi + (deposit * pay_per);
+	}
+
+    return dataset;
+}
+
+function drawDataTable(dataset, table_id) {
+		if ( $.fn.dataTable.isDataTable( table_id ) ) {
+			table = $(table_id).DataTable();
 			table.clear();
 			table.rows.add(dataset);
 			table.draw();
 		}
 		else {
-			$('#example').DataTable( { 
+			$(table_id).DataTable( { 
 				data: dataset,
 				columns: [
 					{ title: "Age" },
@@ -47,5 +51,32 @@ $(document).ready(function() {
 				paging: false
 			});
 		}
+
+}
+
+$(document).ready(function() {
+	$('#button-id').click(function() {
+		var age          = $('#formAge').val();
+		var pay_per      = $('#formPayPeriods').val();
+
+		var value_tax    = $('#formValueTaxable').val();
+		var value_401k   = $('#formValue401k').val();
+		var value_roth   = $('#formValueRoth').val();
+
+		var deposit_tax  = $('#formDepositTaxable').val();
+		var deposit_401k = $('#formDeposit401k').val();
+		var deposit_roth = $('#formDepositRoth').val();
+
+		var dataset = [];
+
+        dataset = futureValue(age, value_tax, deposit_tax, pay_per, 60);
+        drawDataTable(dataset, '#table-tax');
+        
+        dataset = futureValue(age, value_401k, deposit_401k, pay_per, 60);
+        drawDataTable(dataset, '#table-401k');
+
+        dataset = futureValue(age, value_roth, deposit_roth, pay_per, 60);
+        drawDataTable(dataset, '#table-roth');
+
 	});
 });
