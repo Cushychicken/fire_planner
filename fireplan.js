9a5141baad74ba6d2a06ab4b27bd9b69319209dd
Nash Reilly <nrc.reilly@gmail.com>
2017-06-08 20:42:49 -0400

    Added a method of calculating annual deposits (bonus, windfall, etc.)

--- fireplan.js
+++ fireplan.js
@@ -36,84 +36,6 @@ function drawDataTable(dataset, table_id) {
 
 }
 
-function drawInvestmentChart(ages, taxs, swrs) {
-    var ctx = document.getElementById('chart-Investments').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-        labels: ages,
-        datasets: [{
-                // "principal" replaces "apples"
-                label: 'Balance',
-                yAxisID: 'principal',
-                data: taxs,
-                backgroundColor: "rgba(153,255,51,0.6)"
-            }, {
-                // "safe withdrawal" replaces "blues"
-                label: 'Safe Withdrawal Amount',
-                yAxisID: 'swr',
-                type: "line",
-                data: swrs,
-                backgroundColor: "rgba(204,0,204,0.6)"
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    id: 'principal',
-                    position: 'left',
-                    stacked: true
-                }, {
-                    id: 'swr',
-                    position: 'right'
-                }],
-                xAxes: [{
-                    stacked: true
-                }]
-           }
-        }
-    });
-}
-
-function drawSafeWithdrawalChart(ages, taxs, swrs) {
-    var ctx = document.getElementById('chart-swr').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-        labels: ages,
-        datasets: [{
-                // "principal" replaces "apples"
-                label: 'principal',
-                yAxisID: 'principal',
-                data: taxs,
-                backgroundColor: "rgba(153,255,51,0.6)"
-            }, {
-                // "safe withdrawal" replaces "blues"
-                label: 'swr',
-                yAxisID: 'swr',
-                type: "line",
-                data: swrs,
-                backgroundColor: "rgba(204,0,204,0.6)"
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    id: 'principal',
-                    position: 'left',
-                    stacked: true
-                }, {
-                    id: 'swr',
-                    position: 'right'
-                }],
-                xAxes: [{
-                    stacked: true
-                }]
-           }
-        }
-    });
-}
-
 $(document).ready(function() {
 	$('#button-id').click(function() {
 		var age          = $('#formAge').val();
@@ -121,13 +43,17 @@ $(document).ready(function() {
 		var avg_return   = $('#formAvgReturns').val();
 		var swr_rate     = $('#formSafeWithdrawal').val();
 
-		var value_tax    = Number($('#formValueTaxable').val().replace(/[^0-9\.]+/g,""));
-		var value_401k   = Number($('#formValue401k').val().replace(/[^0-9\.]+/g,""));
-		var value_roth   = Number($('#formValueRoth').val().replace(/[^0-9\.]+/g,""));
+		var value_tax			= Number($('#formValueTaxable').val().replace(/[^0-9\.]+/g,""));
+		var value_401k			= Number($('#formValue401k').val().replace(/[^0-9\.]+/g,""));
+		var value_roth			= Number($('#formValueRoth').val().replace(/[^0-9\.]+/g,""));
 
-		var deposit_tax  = Number($('#formDepositTaxable').val().replace(/[^0-9\.]+/g,""));
-		var deposit_401k = Number($('#formDeposit401k').val().replace(/[^0-9\.]+/g,""));
-		var deposit_roth = Number($('#formDepositRoth').val().replace(/[^0-9\.]+/g,""));
+		var deposit_tax			= Number($('#formDepositTaxable').val().replace(/[^0-9\.]+/g,""));
+		var deposit_401k		= Number($('#formDeposit401k').val().replace(/[^0-9\.]+/g,""));
+		var deposit_roth		= Number($('#formDepositRoth').val().replace(/[^0-9\.]+/g,""));
+
+		var deposit_tax_annual  = Number($('#formDepositTaxableAnnual').val().replace(/[^0-9\.]+/g,""));
+		var deposit_401k_annual = Number($('#formDeposit401kAnnual').val().replace(/[^0-9\.]+/g,""));
+		var deposit_roth_annual = Number($('#formDepositRothAnnual').val().replace(/[^0-9\.]+/g,""));
 
 		var dataset = [];
 
@@ -139,16 +65,30 @@ $(document).ready(function() {
         if (swr_rate==null || swr_rate=="") {
             swr_rate = 0.04;
         } 
-        
-        for (var i = age; i < 60; i++) {
+       
+		var i = Number(age); 
+
+        var project_tax  = futureValue(avg_return, value_tax, deposit_tax, pay_per, (i - age));
+        var project_401k = futureValue(avg_return, value_401k, deposit_401k, pay_per, (i - age));
+        var project_roth = futureValue(avg_return, value_roth, deposit_roth, pay_per, (i - age));
+
+        data = [ 
+            i,
+            formatMoney((swr_rate*project_tax)),
+            formatMoney(project_tax),
+            formatMoney(project_401k),
+            formatMoney(project_roth)
+        ];
+        dataset.push(data);
+
+		i = i + 1;
+
+        while ( i < 60 ) {
             var data = [];
 
-            var project_tax  = futureValue(avg_return, value_tax, deposit_tax, pay_per, (i - age));
-            var project_401k = futureValue(avg_return, value_401k, deposit_401k, pay_per, (i - age));
-            var project_roth = futureValue(avg_return, value_roth, deposit_roth, pay_per, (i - age));
-
-            var nocont_401k  = futureValue(avg_return, project_401k, 0, pay_per, (60 - i ));
-            var nocont_roth  = futureValue(avg_return, project_roth, 0, pay_per, (60 - i ));
+            project_tax  = futureValue(avg_return, (project_tax + deposit_tax_annual), deposit_tax, pay_per, 1);
+            project_401k = futureValue(avg_return, (project_401k + deposit_401k_annual), deposit_401k, pay_per, 1);
+            project_roth = futureValue(avg_return, (project_roth + deposit_roth_annual), deposit_roth, pay_per, 1);
 
             data = [ 
                i,
@@ -158,6 +98,8 @@ $(document).ready(function() {
                formatMoney(project_roth)
             ];
             dataset.push(data);
+
+			i = i + 1;
         }
         
         drawDataTable(dataset, "#table-tax");
@@ -165,8 +107,6 @@ $(document).ready(function() {
         var ages = dataset.map(function(value,index) { return value[0]; });
         var taxs = dataset.map(function(value,index) { return value[2].replace(/[^0-9\.]+/g,""); });
         var swrs = dataset.map(function(value,index) { return value[1].replace(/[^0-9\.]+/g,""); });
-        drawInvestmentChart(ages, taxs, swrs);
-        drawSafeWithdrawalChart(ages, taxs, swrs);
 
 	});
 
